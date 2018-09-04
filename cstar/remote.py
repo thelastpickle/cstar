5dea09227b74e7b9b7f5a8d8dd76fb52427ec2f3
Alexander Dejanovski <alex@thelastpickle.com>
2018-09-04 09:59:52 +0200

    Replace paramiko with ssh2-python

--- cstar/remote.py
+++ cstar/remote.py
@@ -12,13 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import paramiko.client
+#import paramiko.client
+from ssh2.session import Session
+from ssh2.sftp import LIBSSH2_FXF_CREAT, LIBSSH2_FXF_WRITE, LIBSSH2_FXF_READ \
+    LIBSSH2_SFTP_S_IRUSR, LIBSSH2_SFTP_S_IRGRP, LIBSSH2_SFTP_S_IWUSR, \
+    LIBSSH2_SFTP_S_IROTH
+import os, socket
 import re
 
 from cstar.output import err, debug, msg
 from cstar.exceptions import BadSSHHost, BadEnvironmentVariable, NoHostsSpecified
 from cstar.executionresult import ExecutionResult
 
+SFTP_MODE = LIBSSH2_SFTP_S_IRUSR | \
+           LIBSSH2_SFTP_S_IWUSR | \
+           LIBSSH2_SFTP_S_IRGRP | \
+           LIBSSH2_SFTP_S_IROTH
+SFTP_FLAGS = LIBSSH2_FXF_CREAT | LIBSSH2_FXF_WRITE
 PING_COMMAND = "echo ping"
 
 _alnum_re = re.compile(r"[^a-zA-Z0-9\|_]")
@@ -35,7 +45,8 @@ class Remote(object):
         self.ssh_username = ssh_username
         self.ssh_password = ssh_password
         self.ssh_identity_file = ssh_identity_file
-        self.client = None
+        self.channel = None
+        self.session = None
 
     def __enter__(self):
         return self
@@ -44,41 +55,41 @@ class Remote(object):
         self.close()
 
     def _connect(self):
-        if self.client:
+        if self.channel:
             # Ensure underlying client is still a valid open connection
             try:
-                stdin, stdout, stderr = self.client.exec_command(PING_COMMAND)
-            except (ConnectionResetError, paramiko.ssh_exception.SSHException):
+                self.channel.exec_command(PING_COMMAND)
+                size, data = self.channel.read()
+                while size > 0:
+                    print(data)
+                    size, data = self.channel.read()
+            except:
                 # ConnectionResetError is raised when a connection was established but then broken
                 # paramiko.ssh_exception.SSHException is raised if the connection was known to be broken
-                self.client = None
+                self.channel = None
 
-        if not self.client:
+        if not self.channel:
             try:
-                self.client = paramiko.client.SSHClient()
-                pkey = None
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((self.hostname, 22))
+                self.session = Session()
+                self.session.handshake(sock)
                 if self.ssh_identity_file != None:
-                    pkey = paramiko.RSAKey.from_private_key_file(self.ssh_identity_file, None)
+                    self.session.userauth_publickey_fromfile(self.ssh_username, None, self.ssh_identity_file, '')
+                if self.ssh_password != None:
+                    self.session.userauth_password(ssh_username, self.ssh_password)
                 debug("Username : ", self.ssh_username)
                 debug("Id file: ", self.ssh_identity_file)
-                self.client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
-                self.client.connect(self.hostname, compress=True, username=self.ssh_username, password=self.ssh_password, pkey=pkey)
+                self.channel = self.session.open_session()
             except:
-                self.client = None
+                self.channel = None
                 raise BadSSHHost("Could not establish an SSH connection to host %s" % (self.hostname,))
 
     def run_job(self, file, jobid, timeout=None, env={}):
         try:
             self._connect()
-
-            transport = self.client.get_transport()
-            session = transport.open_session()
-            paramiko.agent.AgentRequestHandler(session)
-
             dir = ".cstar/remote-jobs/" + jobid
-
             self.run(("mkdir", "-p", dir))
-
             self.put_command(file, "%s/job" % (dir,))
 
             # Manually insert environment into script, since passing env into exec_command leads to it being
@@ -110,14 +121,13 @@ class Remote(object):
     cd %s
     nohup ./wrapper
     """ % (self.escape(dir),)
-
-            stdin, stdout, stderr = self.client.exec_command(cmd, timeout=timeout)
-            stdout.channel.recv_exit_status()
+            self.channel.execute(cmd, timeout=timeout)
+            out, err, status = self.read_channel()
             real_output = self.read_file(dir + "/stdout")
             real_error = self.read_file(dir + "/stderr")
             real_status = int(self.read_file(dir + "/status"))
             return ExecutionResult(cmd, real_status, real_output, real_error)
-        except (ConnectionResetError, paramiko.ssh_exception.SSHException):
+        except:
             raise BadSSHHost("SSH connection to host %s was reset" % (self.hostname,))
 
     def get_job_status(self, jobid):
@@ -128,19 +138,38 @@ class Remote(object):
             self._connect()
             cmd = " ".join(self.escape(s) for s in argv)
 
-            stdin, stdout, stderr = self.client.exec_command(cmd)
-            status = stdout.channel.recv_exit_status()
-            out = stdout.read()
-            error = stderr.read()
+            self.channel.execute(cmd)
+            out, error, status = self.read_channel()
             if status != 0:
                 err("Command %s failed with status %d on host %s" % (cmd, status, self.hostname))
             else:
                 debug("Command %s succeeded on host %s, output was %s and %s" %
                       (cmd, self.hostname, str(out, 'utf-8'), str(error, 'utf-8')))
             return ExecutionResult(cmd, status, str(out, 'utf-8'), str(error, 'utf-8'))
-        except (ConnectionResetError, paramiko.ssh_exception.SSHException):
+        except:
             self.client = None
             raise BadSSHHost("SSH connection to host %s was reset" % (self.hostname,))
+
+    # read stderr, stdout and exit code from the ssh2 channel object
+    def read_channel(self):
+        stdout_list = []
+        stderr_list = []
+        
+        # read stdout
+        size, stdout_part = self.channel.read()
+        while size > 0:
+            stdout_list.append(stdout_part)
+            size, stdout_part = self.channel.read()
+        
+        # read stderr
+        size, stderr_part = self.channel.read_stderr()
+        while size > 0:
+            stderr_list.append(stderr_part)
+            size, stderr_part = self.channel.read_stderr()
+         
+        status = self.channel.get_exit_status()
+
+        return stdout_list.join(""), stderr_list.join(""), status
 
     @staticmethod
     def escape(input):
@@ -150,32 +179,38 @@ class Remote(object):
 
     def read_file(self, remotepath):
         self._connect()
-        with self.client.open_sftp() as ftp_client:
-            with ftp_client.file(remotepath, 'r') as f:
-                return str(f.read(), 'utf-8')
+        content = []
+        sftp = self.session.sftp_init()
+        with sftp.open(remotepath, LIBSSH2_FXF_READ, LIBSSH2_SFTP_S_IRUSR) as remote_fh:
+            for size, data in remote_fh:
+                content.append(data)
+        return str(content.join(""), 'utf-8')
 
     def put_file(self, localpath, remotepath):
         self._connect()
-        with self.client.open_sftp() as ftp_client:
-            ftp_client.put(localpath, remotepath)
+        sftp = self.session.sftp_init()
+        with open(localpath, 'rb') as local_fh, \
+            sftp.open(remotepath, SFTP_FLAGS, SFTP_MODE) as remote_fh:
+            for data in local_fh:
+                remote_fh.write(data)
 
     def put_command(self, localpath, remotepath):
         self._connect()
-        with self.client.open_sftp() as ftp_client:
-            ftp_client.put(localpath, remotepath)
-            ftp_client.chmod(remotepath, 0o755)
+        self.put_file(localpath, remotepath)
+        self.run("chmod 755" + remotepath)
 
     def write_command(self, definition, remotepath):
         self._connect()
-        with self.client.open_sftp() as ftp_client:
-            with ftp_client.open(remotepath, 'w') as f:
-                f.write(definition)
-            ftp_client.chmod(remotepath, 0o755)
+        sftp = self.session.sftp_init()
+        with sftp.open(remotepath, SFTP_FLAGS, SFTP_MODE) as remote_fh:
+            remote_fh.write(definition)
+            self.run("chmod 755" + remotepath)
 
     def mkdir(self, path):
         self.run("mkdir " + path)
 
     def close(self):
-        if self.client:
-            self.client.close()
-        self.client = None
+        if self.channel:
+            self.channel.close()
+        self.channel = None
+        self.session = None
